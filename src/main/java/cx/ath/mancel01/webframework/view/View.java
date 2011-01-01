ff655cc4d343b41862e896c2eb541305947964b1
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2011-01-01 14:42:11 +0100

    remove useless imports

--- src/main/java/cx/ath/mancel01/webframework/view/View.java
+++ src/main/java/cx/ath/mancel01/webframework/view/View.java
@@ -22,8 +22,6 @@ import cx.ath.mancel01.webframework.util.FileUtils.FileGrabber;
 import java.io.ByteArrayOutputStream;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
