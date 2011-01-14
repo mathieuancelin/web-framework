422266cdd28336c7731ad130cc724e9863b35435
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2011-01-14 13:24:14 +0100

    Add a default url callback to loginModule contract

--- core/src/main/java/cx/ath/mancel01/webframework/security/LoginModule.java
+++ core/src/main/java/cx/ath/mancel01/webframework/security/LoginModule.java
@@ -19,6 +19,7 @@ package cx.ath.mancel01.webframework.security;
 
 public interface LoginModule {
     boolean authenticate(String username, String password);
+    String defaultCallbackURL();
     String authenticationFailURL();
     String logoutURL();
 }
