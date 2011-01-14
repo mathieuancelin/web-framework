5388d87b44840d1e0b971ae7735d61a4ac87e479
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2011-01-14 11:37:56 +0100

    Add an utility class to sign messages

---
+++ core/src/main/java/cx/ath/mancel01/webframework/util/SecurityUtils.java
@@ -0,0 +1,55 @@
+/*
+ *  Copyright 2011 mathieu.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+
+package cx.ath.mancel01.webframework.util;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+/**
+ *
+ * @author mathieu
+ */
+public class SecurityUtils {
+
+    private static final char[] HEX_CHARS =
+        {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+
+    public static String sign(String message) {
+        byte[] key = "oiuytredcvbhjnkjhgfdghjkjhgfghjkjhgfghjkjhgf".getBytes();
+        if (key.length == 0) {
+            return message;
+        }
+        try {
+            Mac mac = Mac.getInstance("HmacSHA1");
+            SecretKeySpec signingKey = new SecretKeySpec(key, "HmacSHA1");
+            mac.init(signingKey);
+            byte[] messageBytes = message.getBytes("utf-8");
+            byte[] result = mac.doFinal(messageBytes);
+            int len = result.length;
+            char[] hexChars = new char[len * 2];
+            for (int charIndex = 0, startIndex = 0; charIndex < hexChars.length;) {
+                int bite = result[startIndex++] & 0xff;
+                hexChars[charIndex++] = HEX_CHARS[bite >> 4];
+                hexChars[charIndex++] = HEX_CHARS[bite & 0xf];
+            }
+            return new String(hexChars);
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+}
